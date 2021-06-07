dd838572459085e669ef7ae4c02a7acfb3bf1133
kortoaus <kortoaus@gmail.com>
2021-06-07 12:49:48 +1000

    Test

--- src/Components/Contents.js
+++ src/Components/Contents.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Main from "../screens/Main/index";
-import { SVGIcon } from "./svg/svg";
 
 const Contents = ({ children }) => {
   return (
     <div className="ContentWrap">
-      {children}
-      <BrowserRouter>
-        <Route path="/" exact component={Main} />
-      </BrowserRouter>
+      <div className="CotentInnerWrap">
+        {children}
+        <BrowserRouter>
+          <Route path="/" exact component={Main} />
+        </BrowserRouter>
+      </div>
     </div>
   );
 };
--- src/screens/Main/MainPresenter.js
+++ src/screens/Main/MainPresenter.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const MainPresenter = () => {
-  return <div>무야호</div>;
+  return <Link to="/qwer">"anasdf"</Link>;
 };
 
 export default MainPresenter;
